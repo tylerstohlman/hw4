0629f22459e4cf80d6c9d82ee62a43a04e5e5a26
tylerstohlman <tyler.stohlman@kellogg.northwestern.edu>
2022-03-12 01:47:12 +0000

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ email: params["email"] })
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Nope"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Nope"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/sessions/new"
   end
 end
-  
