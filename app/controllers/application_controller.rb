037ff1ea0b4bb5b673429dc0a58e43d786430711
tylerstohlman <tyler.stohlman@kellogg.northwestern.edu>
2022-03-12 01:08:35 +0000

    login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
